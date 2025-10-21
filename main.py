b56caa1e06a566758c61083791dfe9688ca064bd
Friezel <r1058004@student.thomasmore.be>
2025-10-21 16:08:09 +0200

    background + score font

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
 #Inporteren van pygame
 import pygame
-
 #Initialiseren van pygame
 pygame.init()
+
+#titel
+pygame.display.set_caption('Dr.Mario Clone')
 
 #Schermgrootte & scherm aanmaken
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#player aanmaken (rood vierkant)
-player=pygame.image.load('../sprites/blok rood geel.png').convert()
+#Opzetten van clockset
+clock=pygame.time.Clock()
+
+font=pygame.font.Font("./Python/Pygame/Dr.MarioClone/Fonts/PixelifySans-VariableFont_wght.ttf",50)
 
 #De run variabele
 run = True
 
-x=0
+blok_geel_rood= pygame.image.load("./Python/Pygame/Dr.MarioClone/sprites/blokroodgeel.png").convert()
+background=pygame.image.load("./Python/Pygame/Dr.MarioClone/sprites/background.png").convert()
+kader=pygame.image.load("./Python/Pygame/Dr.MarioClone/sprites/kader.png")
+text_surface=font.render("Score",False,"White")
 
 #Gameloop
 while(run==True):
-    #Zet scherm in het zwart
     screen.fill((0,0,0))
-    
-    #Tekend het vierkant
-    screen.blit(player,(x,30))
-
-    #Maakt het mogelijk om vierkant te bewegen
-    key=pygame.key.get_pressed()
-    if(key[pygame.K_q]==True):
-        player.move_ip(-1,0)
-    elif(key[pygame.K_d]==True):
-        player.move_ip(1,0)
-    elif(key[pygame.K_z]==True):
-        player.move_ip(0,-1)
-    elif(key[pygame.K_s]==True):
-        player.move_ip(0,1)
+    screen.blit(background,(0,0))
+    screen.blit(kader,(-100,0))
+    screen.blit(text_surface,(500,200))
 
     #Event checker die nakijkt of er op de x is gedrukt om het programma uit te zetten
     for event in pygame.event.get():
@@ -43,5 +38,7 @@ while(run==True):
 
     #Refrech het display
     pygame.display.update()
+
+    clock.tick(60)
 #Zet pygame uit nadat de loop uit is
 pygame.quit
